eaa932c998de01fbf3d945fc2f8f5ec2d81d9bfa
wwwater <daria.shubina@gmail.com>
2017-03-15 13:38:21 -0300

    Verify JWT token on POST request

--- backend/src/Api.hs
+++ backend/src/Api.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Api where
 
@@ -7,15 +8,16 @@ import qualified Model as M
 import qualified Storage as S
 
 import Data.Aeson
-import Control.Monad.IO.Class     (MonadIO, liftIO)
+import Control.Monad.IO.Class               (MonadIO, liftIO)
 import Control.Monad.Trans.Either
 import Servant
+import Servant.Server.Internal.ServantErr   (ServantErr, err401, err404)
 import Database.SQLite.Simple as Sql
-import Crypto.PasswordStore (verifyPassword)
-import Data.ByteString.Char8 (pack)
-import Data.Text             (unpack)
+import Crypto.PasswordStore                 (verifyPassword)
+import Data.ByteString.Char8                (pack)
+import Data.Text                            (unpack)
 
-import Jwt
+import Jwt                                  (createJwt, verifyJwt)
 
 instance FromJSON M.Credentials
 instance ToJSON M.Jwt
@@ -32,18 +34,20 @@ jwtServer :: Sql.Connection -> Server JwtAPI
 jwtServer conn =
   grantJwt
     where
-      grantJwt credentials = liftIOMaybeToEither err401 $
-        issueJwt (M.password credentials) $ S.getUserPassword conn (M.username credentials)
+      passwordHash credentials = S.getUserPassword conn (M.username credentials)
+      jwt credentials = issueJwt (M.password credentials) (passwordHash credentials)
+      error = err401 { errBody = "Wrong password or user does not exist."}
+      grantJwt credentials = liftIOMaybeToEither error (jwt credentials)
 
 issueJwt :: String -> IO (Maybe String) -> IO (Maybe M.Jwt)
-issueJwt password hashIO = do
-    hash <- liftIO hashIO
-    case hash of
-        Just passwordHash ->
-            if verifyPassword (pack password) (pack passwordHash)
-                then fmap (Just . M.Jwt . unpack) $ createJwt "meow"
-                else return Nothing
-        Nothing -> return Nothing
+issueJwt password passwordHashIO = do
+  maybePasswordHash <- liftIO passwordHashIO
+  case maybePasswordHash of
+    Just passwordHash ->
+      if verifyPassword (pack password) (pack passwordHash)
+        then fmap (Just . M.Jwt . unpack) $ createJwt "meow"
+        else return Nothing
+    Nothing -> return Nothing
 
 
 
@@ -52,7 +56,7 @@ issueJwt password hashIO = do
 type PostAPI =
        Get '[JSON] [M.Post]
   :<|> Capture "postId" Int :> Get '[JSON] M.Post
-  :<|> ReqBody '[JSON] M.Post :> Post '[JSON] M.Post
+  :<|> Header "jwt" String :> ReqBody '[JSON] M.Post :> Post '[JSON] M.Post
 
 
 postServer :: Sql.Connection -> Server PostAPI
@@ -62,10 +66,23 @@ postServer conn =
   where
     getAllPosts = liftIO $ S.selectAllPosts conn
     getPost postId = liftIOMaybeToEither err404 $ S.selectPost conn postId
-    updatePost post = liftIOMaybeToEither err404 $
-        case M.postId post of
-            Just postId -> S.updatePost conn post
-            Nothing -> S.insertPost conn post
+    updatePost jwt post =
+      case jwt of
+        Just jwtToken -> do
+          valid <- liftIO $ verifyJwt "meow" jwtToken
+          if valid
+            then updateAuthorizedPost conn post
+            else left err401 { errBody = "JWT token has expired or not valid." }
+        Nothing -> left err401 { errBody = "Please provide JWT token in header." }
+
+
+
+updateAuthorizedPost :: Sql.Connection -> M.Post -> EitherT ServantErr IO M.Post
+updateAuthorizedPost conn post = liftIOMaybeToEither err404 $
+  case M.postId post of
+    Just postId -> S.updatePost conn post
+    Nothing -> S.insertPost conn post
+
 
 
 
@@ -73,10 +90,10 @@ postServer conn =
 
 liftIOMaybeToEither ::  (MonadIO m) => a -> IO (Maybe b) -> EitherT a m b
 liftIOMaybeToEither err x = do
-    m <- liftIO x
-    case m of
-      Nothing -> left err
-      Just x -> right x
+  m <- liftIO x
+  case m of
+    Nothing -> left err
+    Just x -> right x
 
 
 
--- backend/src/Jwt.hs
+++ backend/src/Jwt.hs
@@ -1,17 +1,38 @@
-{-#LANGUAGE TemplateHaskell #-}
 {-#LANGUAGE OverloadedStrings #-}
-{-#LANGUAGE FlexibleInstances #-}
+
 module Jwt where
 
-import Web.JWT                  (iat, exp, secret, encodeSigned, Algorithm(HS256), def, JSON, intDate)
+import Web.JWT                  (iat,
+                                exp,
+                                secret,
+                                encodeSigned,
+                                decodeAndVerifySignature,
+                                Algorithm(HS256),
+                                def,
+                                JSON,
+                                intDate,
+                                claims,
+                                secondsSinceEpoch)
 import Data.Time.Clock.POSIX    (getPOSIXTime)
 import Control.Monad.IO.Class   (liftIO)
 import Data.Text                (pack)
 
 createJwt :: String -> IO JSON
 createJwt key = do
-    now <- getPOSIXTime
-    let expire = now + 60 * 60
-        claimsSet = def { iat = intDate now, Web.JWT.exp = intDate expire }
-     in return $ encodeSigned HS256 (secret (pack key)) claimsSet
+  now <- getPOSIXTime
+  let expire = now + 60 * 60
+      claimsSet = def { iat = intDate now, Web.JWT.exp = intDate expire }
+   in return $ encodeSigned HS256 (secret (pack key)) claimsSet
+
+verifyJwt :: String -> String -> IO Bool
+verifyJwt key jwt =
+  case decodeAndVerifySignature (secret (pack key)) (pack jwt) of
+    Just verifiedJwt ->
+      case Web.JWT.exp $ claims verifiedJwt of
+        Just expiredTime -> do
+          now <- getPOSIXTime
+          let expire = secondsSinceEpoch expiredTime
+            in if now > expire then return False else return True
+        Nothing -> return False
+    Nothing -> return False
 
